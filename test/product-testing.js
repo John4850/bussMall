ebbc7e5ea8660af39cd8d3368beac60d2ebaf3e3
JOHN NELSON <JohnNelson4850@gmail.com>
2019-07-31 09:29:49 -0700

    slice of bootstrapped data tests

--- test/product-testing.js
+++ test/product-testing.js
@@ -1,5 +1,6 @@
 import store from '../data/store.js';
 import productList from '../data/product-list.js';
+import ProductSet from '../data/product-set.js';
 
 const test = QUnit.test;
 store.storage = window.sessionStorage;
@@ -8,4 +9,13 @@ test ('Bootstraps Products to Store', assert => {
     const products = store.getProducts();
 
     assert.deepEqual(products, productList);
+});
+
+test ('Copies bootstrapped data', assert =>{
+
+    const products = new ProductSet(productList);
+
+
+    assert.deepEqual(products.list, productList);
+    assert.notEqual(products.list, productList);
 });
